04bfd0a330abbac19efd027353ae33e2de9e1305
Congb19 <347006087@qq.com>
2018-10-18 19:45:33 +0800

    10.18 randomBack()

---
+++ greeter.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello, " + person + "!";
+}
+var user = "Congb19";
+document.body.innerHTML = greeter(user);
---
+++ greeter.ts
@@ -0,0 +1,7 @@
+function greeter(person) {
+    return "Hello, " + person + "!";
+}
+
+let user = "Congb19";
+
+document.body.innerHTML = greeter(user);
